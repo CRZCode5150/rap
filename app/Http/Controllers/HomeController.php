90765111ec2972fc42ca538249b19875b41ae5e9
Josue Plaza <plazajosue2@gmail.com>
2018-04-04 16:12:46 -0500

    register users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Hash;
 
 class HomeController extends Controller
 {
@@ -29,17 +30,25 @@ class HomeController extends Controller
 
     public function register(Request $request)
     {
+
+        if ($request->CreateYourPassword != $request->ConfirmYourPassword) {
+            return back();
+        }
+
+
         $newUser = new User;
-        $newUser->password = $request->CreateYourPassword;
+        $newUser->password = Hash::make($request->CreateYourPassword);
         $newUser->username= $request->username;
         $newUser->firstname = $request->firstname;
         $newUser->lastname = $request->lastname;
         $newUser->city = $request->city;
         $newUser->state = $request->state;
         $newUser->zipcode = $request->zipcode;
-        
-        // Get all that data that the user inputted
-        // log in
-        // go to the homepage
+        $newUser->save();
+
+        if (Auth::attempt(['username' => $request->username, 'password' => $password])) {
+            // Authentication passed...
+            return redirect()->intended('home');
+        }
     }
 }
